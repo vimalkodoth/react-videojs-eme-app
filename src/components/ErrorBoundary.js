fab698dacb266f6aca1813112ab8d6f0fd22d4aa
Vimal Madhavan <vimal.madhavan@intigral.net>
2020-10-19 15:54:41 +0400

    ErrorBoundary improved

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes, { node } from "prop-types";
+import { css } from "@emotion/core";
 
 /** Error Boundary Component */
 class ErrorBoundary extends Component {
@@ -26,8 +27,52 @@ class ErrorBoundary extends Component {
     }
 
     render() {
+        const { hasError, errorInfo } = this.state;
+        if (hasError) {
+            return (
+                <div css={ErrorStyles}>
+                    <div>
+                        <div className="error-header">
+                            <p>
+                                There was an error in loading this page.{" "}
+                                <span
+                                    style={{
+                                        cursor: "pointer",
+                                        color: "#0077FF"
+                                    }}
+                                    onClick={() => {
+                                        window.location.reload();
+                                    }}
+                                >
+                                    Reload this page
+                                </span>{" "}
+                            </p>
+                        </div>
+                        <div className="error-body">
+                            <details className="error-details">
+                                <summary>Click for error details</summary>
+                                {errorInfo &&
+                                    errorInfo.componentStack.toString()}
+                            </details>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
         return this.props.children;
     }
 }
 
 export default ErrorBoundary;
+
+const ErrorStyles = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: inherit;
+    min-height: 100vh;
+
+    & > div {
+        max-width: 420px;
+    }
+`;
